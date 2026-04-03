e24a8b801fb80f7e5745d43f721eb0c3d6d20d0f
Miner <285669321@qq.com>
2026-04-03 21:19:35 +0800

    准备修改生命周期管理，谁持有谁

---
+++ include/manager/PEBFactory.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class VoidPhase;
+class MidPhase;
+class BossPhase;
+class SpellPhase;
+class Enemy;
+class Behavior;
+
+class PEBFactory
+{
+    
+};
+
+//PEBFactory意思是Phase,Enemy,Behavior的Factory
+//负责通过package创建最基本的空的P,E,B
+
