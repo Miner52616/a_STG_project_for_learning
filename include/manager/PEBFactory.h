3aaac7df7cc8dcd9f908aefc4099f470b9715334
Miner <285669321@qq.com>
2026-04-03 21:20:09 +0800

    准备修改生命周期管理，谁持有谁

--- include/manager/PEBFactory.h
+++ include/manager/PEBFactory.h
@@ -7,10 +7,12 @@ class SpellPhase;
 class Enemy;
 class Behavior;
 
+/*
 class PEBFactory
 {
     
 };
+*/
 
 //PEBFactory意思是Phase,Enemy,Behavior的Factory
 //负责通过package创建最基本的空的P,E,B
--- src/manager/ScriptLoader.cpp
+++ src/manager/ScriptLoader.cpp
@@ -23,32 +23,71 @@ std::unique_ptr<Phase> ScriptLoader::loadPhase(const std::string& path)
 
     if(type=="MidPhase")
     {
-        createMidPhase();
+        std::unique_ptr<MidPhase> phase=buildMidPhase(phasescript);
+        return std::move(phase);
     }
 }
 
-std::unique_ptr<MidPhase> ScriptLoader::createMidPhase()
+std::unique_ptr<MidPhase> ScriptLoader::buildMidPhase(const sol::table& phasescript)
 {
-    sol::table phasescript=lua_.lua_["phase"];
-
-    std::unique_ptr<MidPhase> phase=std::make_unique<MidPhase>(resource_,yellowpage_,phasescript["time"]);
+    std::unique_ptr<MidPhase> phase=PEBfactory_.createMidPhase(phasescript);
 
     sol::table enemytable=phasescript["enemytable"];
     int enemytablesize=enemytable.size();
     for(int i=1;i<=enemytablesize;i++)
     {
         sol::table enemyscript=enemytable[i];
-        std::unique_ptr<Enemy> enemy=std::make_unique<Enemy>(resource_->app_.enemyTexture_);
-        enemy->setPosition({(float)enemyscript["x"],(float)enemyscript["y"]});
-        enemy->setHP(enemyscript["HP"]);
-        enemy->set_start_end(enemyscript["start"],enemyscript["end"]);
-        
-        sol::table behaviortable=enemyscript["behaviortable"];
-        int behaviortablesize=behaviortable.size();
-        for(int j=1;j<=behaviortablesize;j++)
-        {
-            ;
-        }
-        
+        std::unique_ptr<Enemy> enemy=buildEnemy(enemyscript);
     }
+
+    return std::move(phase);
+}
+
+std::unique_ptr<Enemy> ScriptLoader::buildEnemy(const sol::table& enemyscript)
+{
+    std::unique_ptr<Enemy> enemy=PEBfactory_.createEnemy(enemyscript);
+    enemy->setPosition({(float)enemyscript["x"],(float)enemyscript["y"]});
+    enemy->setHP(enemyscript["HP"]);
+    enemy->set_start_end(enemyscript["start"],enemyscript["end"]);
+
+    sol::table behaviortable=enemyscript["behaviortable"];
+    int behaviortablesize=behaviortable.size();
+    for(int i=1;i<=behaviortablesize;i++)
+    {
+        sol::table behaviorscript=behaviortable[i];
+        std::unique_ptr<Behavior> behavior=buildBehavior(behaviorscript);
+    }
+
+    return std::move(enemy);
+}
+
+std::unique_ptr<Behavior> ScriptLoader::buildBehavior(const sol::table& behaviorscript)
+{
+    std::unique_ptr<Behavior> behavior=PEBfactory_.createBehavior(behaviorscript);
+
+
+    return std::move(behavior);
+}
+
+//**********************************************
+//以下为PEBFactory
+//***********************************************
+
+std::unique_ptr<MidPhase> PEBFactory::createMidPhase(const sol::table& phasescript)
+{
+    std::unique_ptr<MidPhase> phase=std::make_unique<MidPhase>(resource_,yellowpage_,phasescript["time"]);
+    return std::move(phase);
+}
+
+std::unique_ptr<Enemy> PEBFactory::createEnemy(const sol::table& enemyscript)
+{
+    std::unique_ptr<Enemy> enemy=std::make_unique<Enemy>(resource_->app_.enemyTexture_);
+    return std::move(enemy);
+}
+
+std::unique_ptr<Behavior> PEBFactory::createBehavior(const sol::table& behaviorscript)
+{
+    std::unique_ptr<Behavior> behavior;
+    
+    return std::move(behavior); 
 }
