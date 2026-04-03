d709760533dc39fc3bd6b88e16db86ecce35b31c
Miner <285669321@qq.com>
2026-04-03 21:20:09 +0800

    准备修改生命周期管理，谁持有谁

--- include/manager/ScriptLoader.h
+++ include/manager/ScriptLoader.h
@@ -5,6 +5,8 @@
 class Resource;
 class YellowPage;
 class MidPhase;
+class Enemy;
+class Behavior;
 
 class ScriptLoader
 {
@@ -12,11 +14,27 @@ private:
     LuaManager& lua_;
     Resource* resource_;
     YellowPage* yellowpage_;
+    PEBFactory PEBfactory_;
 
 private:
-    std::unique_ptr<MidPhase> createMidPhase();
+    std::unique_ptr<MidPhase> buildMidPhase(const sol::table& phasescript);
+    std::unique_ptr<Enemy> buildEnemy(const sol::table& enemyscript);
+    std::unique_ptr<Behavior> buildBehavior(const sol::table& behaviorscript);
 
 public:
     ScriptLoader(LuaManager& lua,Resource* resource,YellowPage* yellowpage);
     std::unique_ptr<Phase> loadPhase(const std::string& path);
+};
+
+class PEBFactory
+{
+private:
+    Resource* resource_;
+    YellowPage* yellowpage_;
+
+public:
+    std::unique_ptr<MidPhase> createMidPhase(const sol::table& phasescript);
+    std::unique_ptr<Enemy> createEnemy(const sol::table& enemyscript);
+    std::unique_ptr<Behavior> createBehavior(const sol::table& behaviorscript);
+
 };
