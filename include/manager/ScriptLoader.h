8d88d6e5b13c049a490fc19348dab4cbd0b4fdf1
Miner <285669321@qq.com>
2026-04-04 13:22:02 +0800

    优化脚本阅读程序的递归结构以及对象解耦

--- include/manager/ScriptLoader.h
+++ include/manager/ScriptLoader.h
@@ -17,18 +17,71 @@ private:
     
 
 public:
-    using BehaviorCreator=std::function<std::unique_ptr<Behavior>(Enemy*, sol::table)>;
+    using BehaviorCreator=std::function<std::unique_ptr<Behavior>(Enemy*,const sol::table&)>;
+    using PhaseCreator=std::function<std::unique_ptr<Phase>(const sol::table& phasescript)>;
 private:
-    std::unordered_map<std::string, BehaviorCreator> registry_;
+    std::unordered_map<std::string,BehaviorCreator> behaviorregistry_;
+    std::unordered_map<std::string,PhaseCreator> phaseregistry_;
 public:
     PEBFactory();
-    void registerBehavior(const std::string& name, BehaviorCreator creator);
+    void registerBehavior(const std::string& name,BehaviorCreator creator);
+    void registerPhase(const std::string& name,PhaseCreator creator);
     void setResource(Resource* resource);
     void setYellowPage(YellowPage* yellowpage);
-    std::unique_ptr<MidPhase> createMidPhase(const sol::table& phasescript);
+    std::unique_ptr<Phase> createPhase(const sol::table& phasescript);
     std::unique_ptr<Enemy> createEnemy(const sol::table& enemyscript);
     std::unique_ptr<Behavior> createBehavior(Enemy* enemy,const sol::table& behaviorscript);
     //std::unique_ptr<Behavior> createBehavior(const sol::table& behaviorscript);
+};
+
+class BFactory
+{
+private:
+    Resource* resource_;
+    YellowPage* yellowpage_;
+
+public:
+    using BehaviorCreator=std::function<std::unique_ptr<Behavior>(Enemy*,const sol::table&)>;
+private:
+    std::unordered_map<std::string,BehaviorCreator> behaviorregistry_;
+public:
+    BFactory();
+    void registerBehavior(const std::string& name,BehaviorCreator creator);
+    void setResource(Resource* resource);
+    void setYellowPage(YellowPage* yellowpage);
+    std::unique_ptr<Behavior> buildBehavior(Enemy* enemy,const sol::table& behaviorscript);
+}; 
+
+class EFactory
+{
+private:
+    Resource* resource_;
+    YellowPage* yellowpage_;
+    BFactory Bfactory_;
+
+public:
+    void setResource(Resource* resource);
+    void setYellowPage(YellowPage* yellowpage);
+    std::unique_ptr<Enemy> buildEnemy(const sol::table& enemyscript);
+};
+
+class PFactory
+{
+private:
+    Resource* resource_;
+    YellowPage* yellowpage_;
+    EFactory Efactory_;
+
+public:
+    using PhaseCreator=std::function<std::unique_ptr<Phase>(const sol::table& phasescript)>;
+private:
+    std::unordered_map<std::string,PhaseCreator> phaseregistry_;
+public:
+    PFactory();
+    void registerPhase(const std::string& name,PhaseCreator creator);
+    void setResource(Resource* resource);
+    void setYellowPage(YellowPage* yellowpage);
+    std::unique_ptr<Phase> buildPhase(const sol::table& phasescript);
 };
 
 class ScriptLoader
@@ -38,16 +91,21 @@ private:
     Resource* resource_;
     YellowPage* yellowpage_;
     PEBFactory PEBfactory_;
+    PFactory Pfactory_;
 
+public:
+    using PhaseCreator=std::function<std::unique_ptr<Phase>(const sol::table&)>;
 private:
-    std::unique_ptr<MidPhase> buildMidPhase(const sol::table& phasescript);
+    std::unordered_map<std::string,PhaseCreator> phaseregistry_;
+private:
+    std::unique_ptr<Phase> buildPhase(const sol::table& phasescript);
     std::unique_ptr<Enemy> buildEnemy(const sol::table& enemyscript);
     std::unique_ptr<Behavior> buildBehavior(Enemy* enemy,const sol::table& behaviorscript);
 
 public:
     ScriptLoader(LuaManager& lua);
+    void registerPhase(const std::string& name,PhaseCreator creator);
     void setResource(Resource* resource);
     void setYellowPage(YellowPage* yellowpage);
-    std::unique_ptr<Phase> buildPhase(const sol::table& phasescript);
     void loadPhase(const std::string& path);
 }; 
