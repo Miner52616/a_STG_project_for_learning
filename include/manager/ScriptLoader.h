598da33a7879d82d0c123258b3636c1dfd1f1cb4
Miner <285669321@qq.com>
2026-04-04 13:47:56 +0800

    重构读脚本程序，强调递归结构

--- include/manager/ScriptLoader.h
+++ include/manager/ScriptLoader.h
@@ -9,31 +9,6 @@ class MidPhase;
 class Enemy;
 class Behavior;
 
-class PEBFactory
-{
-private:
-    Resource* resource_;
-    YellowPage* yellowpage_;
-    
-
-public:
-    using BehaviorCreator=std::function<std::unique_ptr<Behavior>(Enemy*,const sol::table&)>;
-    using PhaseCreator=std::function<std::unique_ptr<Phase>(const sol::table& phasescript)>;
-private:
-    std::unordered_map<std::string,BehaviorCreator> behaviorregistry_;
-    std::unordered_map<std::string,PhaseCreator> phaseregistry_;
-public:
-    PEBFactory();
-    void registerBehavior(const std::string& name,BehaviorCreator creator);
-    void registerPhase(const std::string& name,PhaseCreator creator);
-    void setResource(Resource* resource);
-    void setYellowPage(YellowPage* yellowpage);
-    std::unique_ptr<Phase> createPhase(const sol::table& phasescript);
-    std::unique_ptr<Enemy> createEnemy(const sol::table& enemyscript);
-    std::unique_ptr<Behavior> createBehavior(Enemy* enemy,const sol::table& behaviorscript);
-    //std::unique_ptr<Behavior> createBehavior(const sol::table& behaviorscript);
-};
-
 class BFactory
 {
 private:
@@ -90,21 +65,10 @@ private:
     LuaManager& lua_;
     Resource* resource_;
     YellowPage* yellowpage_;
-    PEBFactory PEBfactory_;
     PFactory Pfactory_;
 
 public:
-    using PhaseCreator=std::function<std::unique_ptr<Phase>(const sol::table&)>;
-private:
-    std::unordered_map<std::string,PhaseCreator> phaseregistry_;
-private:
-    std::unique_ptr<Phase> buildPhase(const sol::table& phasescript);
-    std::unique_ptr<Enemy> buildEnemy(const sol::table& enemyscript);
-    std::unique_ptr<Behavior> buildBehavior(Enemy* enemy,const sol::table& behaviorscript);
-
-public:
     ScriptLoader(LuaManager& lua);
-    void registerPhase(const std::string& name,PhaseCreator creator);
     void setResource(Resource* resource);
     void setYellowPage(YellowPage* yellowpage);
     void loadPhase(const std::string& path);
